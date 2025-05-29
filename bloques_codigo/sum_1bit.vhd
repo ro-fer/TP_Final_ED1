99a88b4f297a23f43d168622d9a4810a885c93b3
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-05-29 11:40:52 -0300

    Update sum_1bit.vhd

--- bloques_codigo/sum_1bit.vhd
+++ bloques_codigo/sum_1bit.vhd
@@ -15,7 +15,7 @@ entity sum_1b is
   );
 end;
 
-architecture sumb_1b_arq of sum_1b is
+architecture sum_1b_arq of sum_1b is
 begin
   s_o <= a_i xor b_i xor c_i; -- suma con xor
   c_o <= (a_i and b_i) or (a_i and c_i) or (b_i and c_i); -- salida del Carry
