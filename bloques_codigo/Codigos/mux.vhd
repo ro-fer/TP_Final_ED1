109f6852503e24b54005fde9a4e1453a6f43920e
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-14 15:53:19 -0300

    Update mux.vhd

--- bloques_codigo/Codigos/mux.vhd
+++ bloques_codigo/Codigos/mux.vhd
@@ -12,18 +12,18 @@ entity mux is
     pix_x_i    : in std_logic_vector(9 downto 0);
     pix_y_i    : in std_logic_vector(9 downto 0);
     video_on_i : in std_logic;
-    char0      : in std_logic_vector(6 downto 0);
-    char1      : in std_logic_vector(6 downto 0);
-    char2      : in std_logic_vector(6 downto 0);
-    char3      : in std_logic_vector(6 downto 0);
-    char4      : in std_logic_vector(6 downto 0);
+    char0      : in std_logic_vector(7 downto 0);
+    char1      : in std_logic_vector(7 downto 0);
+    char2      : in std_logic_vector(7 downto 0);
+    char3      : in std_logic_vector(7 downto 0);
+    char4      : in std_logic_vector(7 downto 0);
     pixel_o    : out std_logic
   );
 end entity;
 
 architecture rtl of mux is
   -- Subdivisiones 
-  signal char_sel     : std_logic_vector(6 downto 0); -- Caracter seleccionado
+  signal char_sel     : std_logic_vector(7 downto 0); -- Caracter seleccionado
   signal char_addr    : std_logic_vector(3 downto 0); -- 5 caracteres ? 0 a 4
   signal sub_fila     : std_logic_vector(2 downto 0); -- Y interna del caracter (bits 6-4)
   signal sub_col      : std_logic_vector(2 downto 0); -- X interna del caracter (bits 2-0)
@@ -42,7 +42,7 @@ begin
                 char2 when "0010",
                 char3 when "0011",
                 char4 when "0100",
-                "0000000" when others;
+                "00000000" when others;
 
   -- Instancia de la ROM de caracteres
   rom_inst: entity work.rom
