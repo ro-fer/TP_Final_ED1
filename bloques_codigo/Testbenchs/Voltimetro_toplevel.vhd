a9539982fb5a2baaeb6b5753968bdc12b27ffbcd
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-14 16:12:55 -0300

    Update Voltimetro_toplevel.vhd

--- bloques_codigo/Testbenchs/Voltimetro_toplevel.vhd
+++ bloques_codigo/Testbenchs/Voltimetro_toplevel.vhd
@@ -1,39 +1,41 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity Voltimetro_toplevel_tb is
-end;
+end entity;
 
 architecture sim of Voltimetro_toplevel_tb is
 
+    -- Componente bajo prueba (DUT)
     component Voltimetro_toplevel is
         port(
-            clk_i           : in bit;
-            rst_i           : in bit;
-            data_volt_in_i  : in bit;
-            data_volt_out_o : out bit;
-            hs_o            : out bit;
-            vs_o            : out bit;
-            red_o           : out bit;
-            grn_o           : out bit;
-            blu_o           : out bit
+            clk_i           : in std_logic;
+            rst_i           : in std_logic;
+            data_volt_in_i  : in std_logic;
+            data_volt_out_o : out std_logic;
+            hs_o            : out std_logic;
+            vs_o            : out std_logic;
+            red_o           : out std_logic;
+            grn_o           : out std_logic;
+            blu_o           : out std_logic
         );
     end component;
 
-    -- Señales internas
-    signal clk_tb           : bit := '0';
-    signal rst_tb           : bit := '1';
-    signal data_volt_in_tb  : bit := '0';
-    signal data_volt_out_tb : bit;
-    signal hs_tb            : bit;
-    signal vs_tb            : bit;
-    signal red_tb           : bit;
-    signal grn_tb           : bit;
-    signal blu_tb           : bit;
+    -- Señales internas para conectar al DUT
+    signal clk_tb           : std_logic := '0';
+    signal rst_tb           : std_logic := '1';
+    signal data_volt_in_tb  : std_logic := '0';
+    signal data_volt_out_tb : std_logic;
+    signal hs_tb            : std_logic;
+    signal vs_tb            : std_logic;
+    signal red_tb           : std_logic;
+    signal grn_tb           : std_logic;
+    signal blu_tb           : std_logic;
 
 begin
 
-    -- Instanciar el DUT
+    -- Instancia del DUT
     DUT: Voltimetro_toplevel
         port map (
             clk_i           => clk_tb,
@@ -47,7 +49,7 @@ begin
             blu_o           => blu_tb
         );
 
-    -- Clock de 100 MHz
+    -- Generador de clock de 100 MHz (10 ns de período)
     clk_process : process
     begin
         while true loop
@@ -58,15 +60,15 @@ begin
         end loop;
     end process;
 
-    -- Estímulos de entrada
+    -- Proceso de estímulo
     stim_proc : process
     begin
-        -- Reset activo unos ciclos
+        -- Reset activo por 50 ns
         wait for 50 ns;
         rst_tb <= '0';
 
-        -- Generamos una señal de ADC cuadrada
-        for i in 0 to 500 loop
+        -- Simular señal cuadrada de ADC durante 2 ms
+        for i in 0 to 999 loop
             data_volt_in_tb <= '1';
             wait for 100 ns;
             data_volt_in_tb <= '0';
