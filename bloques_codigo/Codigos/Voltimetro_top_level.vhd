14829e8cf0cbd5a815a10395b9d9a0e4bfcbc8c1
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-14 16:12:57 -0300

    Update Voltimetro_top_level.vhd

--- bloques_codigo/Codigos/Voltimetro_top_level.vhd
+++ bloques_codigo/Codigos/Voltimetro_top_level.vhd
@@ -7,90 +7,86 @@
 -- Fecha: 		01/09/2020
 --              Actualizado: 26/05/2025
 ----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity Voltimetro_toplevel is
-	port(
-		clk_i			: in bit;
-		rst_i			: in bit;
-		data_volt_in_i	: in bit;
-		data_volt_out_o	: out bit;
-		hs_o 			: out bit;
-		vs_o 			: out bit;
-		red_o 			: out bit;
-		grn_o 			: out bit;
-        blu_o 			: out bit
-	);
-	
+    port(
+        clk_i           : in std_logic;
+        rst_i           : in std_logic;
+        data_volt_in_i  : in std_logic;
+        data_volt_out_o : out std_logic;
+        hs_o            : out std_logic;
+        vs_o            : out std_logic;
+        red_o           : out std_logic;
+        grn_o           : out std_logic;
+        blu_o           : out std_logic
+    );
 end Voltimetro_toplevel;
 
-rchitecture Voltimetro_toplevel_arq of Voltimetro_toplevel is
+architecture Voltimetro_toplevel_arq of Voltimetro_toplevel is
 
-	-------------------------------------------------------------------------------------
-	-- Declaracion del componente voltimetro
-	--   Este componente incluye el bloque de procesamiento de datos y control y el
-	--   flip-flop de entrada
-	-------------------------------------------------------------------------------------
-	component Voltimetro is
-		port(
-			clk_i      		: in bit;
-			rst_i      		: in bit;
-			data_volt_in_i  : in bit;
-			data_volt_out_o : out bit;
-			hs_o    		: out bit;
-			vs_o    		: out bit;
-			red_o  			: out bit;
-			grn_o  			: out bit;
-			blu_o  			: out bit
-		);
-	end component Voltimetro;
-	
-	-------------------------------------------------------------------------------------
-	-- Declaracion del componente generador de reloj
-	--   Se implementa con un contador de 2 bits, tomando el bit mas alto de su salida
-	-------------------------------------------------------------------------------------
-	component cont_bin is
-		generic(
-			N: natural := 4	-- Cantidad de bits de la cuenta
-		)
-		port(
-			clk_i : in bit;
-			rst_i : in bit;
-			ena_i : in bit;
-	  		q_o   : out bit
-		);
+    -- Componente Voltimetro
+    component Voltimetro is
+        port(
+            clk_i           : in std_logic;
+            rst_i           : in std_logic;
+            data_volt_in_i  : in std_logic;
+            data_volt_out_o : out std_logic;
+            hs_o            : out std_logic;
+            vs_o            : out std_logic;
+            red_o           : out std_logic;
+            grn_o           : out std_logic;
+            blu_o           : out std_logic
+        );
     end component;
-    
-	signal cuenta   : bit_vector(1 downto 0);
-	signal clk25MHz	: bit;
-	
+
+    -- Componente contador binario
+    component cont_bin is
+        generic(
+            N : natural := 2
+        );
+        port(
+            clk_i : in std_logic;
+            rst_i : in std_logic;
+            ena_i : in std_logic;
+            q_o   : out std_logic_vector(N-1 downto 0)
+        );
+    end component;
+
+    -- Señales internas
+    signal cuenta    : std_logic_vector(1 downto 0);
+    signal clk25MHz  : std_logic;
+
 begin
 
-	-- Generador del reloj lento
-	clk25MHz_gen : cont_bin
-		generic map(
-			N => 2 
-		)
-   		port map ( 
-   			clk_i => clk_i,		-- reloj del sistema (100 MHz)
-   			rst_i => rst_i, 
-			ena_i => '1',
-			q_o   => cuenta
- 		);
-		
-	clk25MHz <= cuenta(1);		-- reloj generado (25 MHz)
+    -- Generador de reloj de 25 MHz (a partir de 100 MHz)
+    clk25MHz_gen : cont_bin
+        generic map(
+            N => 2
+        )
+        port map (
+            clk_i => clk_i,
+            rst_i => rst_i,
+            ena_i => '1',
+            q_o   => cuenta
+        );
 
-	-- Instancia del bloque voltimetro
-	inst_voltimetro: Voltimetro
-		port map(
-            clk_i			=> clk25MHz,
-            rst_i			=> rst_i,
-            data_volt_in_i	=> data_volt_in_i,
-            data_volt_out_o	=> data_volt_out_o,
-            hs_o			=> hs_o,
-            vs_o			=> vs_o,
-            red_o			=> red_o,
-            grn_o			=> grn_o,
-            blu_o			=> blu_o
+    clk25MHz <= cuenta(1);  -- División por 4 = 25 MHz
+
+    -- Instancia del voltímetro
+    inst_voltimetro : Voltimetro
+        port map (
+            clk_i           => clk25MHz,
+            rst_i           => rst_i,
+            data_volt_in_i  => data_volt_in_i,
+            data_volt_out_o => data_volt_out_o,
+            hs_o            => hs_o,
+            vs_o            => vs_o,
+            red_o           => red_o,
+            grn_o           => grn_o,
+            blu_o           => blu_o
         );
 
 end Voltimetro_toplevel_arq;
