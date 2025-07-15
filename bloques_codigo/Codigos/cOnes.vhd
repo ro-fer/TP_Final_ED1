6c9969af408b7fc48391b88e13ae7f1b16fc0231
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 16:53:08 -0300

    Update cOnes.vhd

--- bloques_codigo/Codigos/cOnes.vhd
+++ bloques_codigo/Codigos/cOnes.vhd
@@ -27,7 +27,8 @@ architecture cOnes_arq of cOnes is
           clk_bcd : in std_logic;
           ena_bcd : in std_logic;
           rst_bcd : in std_logic;
-          q_bcd   : out std_logic_vector(3 downto 0)
+          q_bcd   : out std_logic_vector(3 downto 0);
+          max     : out std_logic
       );
     end component;
 
@@ -40,6 +41,7 @@ architecture cOnes_arq of cOnes is
     signal salida_bcd6  : std_logic_vector(3 downto 0) := (3 downto 0 => '0');
     signal salida_bcd7  : std_logic_vector(3 downto 0) := (3 downto 0 => '0');
     signal salida_And   : std_logic_vector(5 downto 0) := (5 downto 0 => '0'); -- saida de los and para habilitar bcd q sigue
+    signal max1, max2, max3, max4, max5, max6, max7 : std_logic;
 
 begin
 
@@ -55,55 +57,63 @@ begin
     salida_And(4) <= ((salida_bcd5(3) and not (salida_bcd5(2)) and not (salida_bcd5(1)) and salida_bcd5(0)) and salida_And(3));
     salida_And(5) <= ((salida_bcd6(3) and not (salida_bcd6(2)) and not (salida_bcd6(1)) and salida_bcd6(0)) and salida_And(4));
 
-    bcd1: contador_bcd
+    bcd1: BCD_counter
         port map (
             clk_bcd => clk_unos,
             ena_bcd => ena_unos,
             rst_bcd => reset,
-            q_bcd   => salida_bcd1
+            q_bcd   => salida_bcd1,
+            max => max1
         );
 
-    bcd2: contador_bcd
+    bcd2: BCD_counter
         port map (
             clk_bcd => clk_unos,
             ena_bcd => salida_And(0),
             rst_bcd => reset,
-            q_bcd   => salida_bcd2
+            q_bcd   => salida_bcd2,
+            max => max2
+
         );
 
-    bcd3: contador_bcd
+    bcd3: BCD_counter
         port map (
             clk_bcd => clk_unos,
             ena_bcd => salida_And(1),
             rst_bcd => reset,
-            q_bcd   => salida_bcd3
+            q_bcd   => salida_bcd3,
+            max => max3
+
         );
 
-    bcd4: contador_bcd
+    bcd4: BCD_counter
         port map (
             clk_bcd => clk_unos,
             ena_bcd => salida_And(2),
             rst_bcd => reset,
-            q_bcd   => salida_bcd4
+            q_bcd   => salida_bcd4,
+            max => max4
         );
 
-    bcd5: contador_bcd      
+    bcd5: BCD_counter      
         port map (
             clk_bcd => clk_unos,
             ena_bcd => salida_And(3),
             rst_bcd => reset,
-            q_bcd   => salida_bcd5
+            q_bcd   => salida_bcd5,
+            max => max5
         );
 
-        bcd6: contador_bcd      
+        bcd6: BCD_counter      
         port map (
             clk_bcd => clk_unos,
             ena_bcd => salida_And(4),
             rst_bcd => reset,
-            q_bcd   => salida_bcd6
+            q_bcd   => salida_bcd6,
+            max => max6
         );
                                 -- para el reset
-        bcd7: contador_bcd      
+        bcd7: BCD_counter      
         port map (
             clk_bcd => clk_unos,
             ena_bcd => salida_And(5),
