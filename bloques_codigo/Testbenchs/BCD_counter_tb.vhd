5fe8f7b092071a2cd35a696d9bcfc414d1201a7e
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 16:53:31 -0300

    Update BCD_counter_tb.vhd

--- bloques_codigo/Testbenchs/BCD_counter_tb.vhd
+++ bloques_codigo/Testbenchs/BCD_counter_tb.vhd
@@ -1,37 +1,33 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
 
 entity BCD_counter_tb is
-  generic(N: natural := 4);
 end;
 
-architecture BCD_counter_tb_arq of BCD_counter_tb is
+architecture sim of BCD_counter_tb is
 
   component BCD_counter
     generic(N: natural := 4);
     port(
-      clk_i  : in std_logic;
-      rst_i  : in std_logic;
-      ena_i  : in std_logic;
-      count  : out std_logic_vector(N-1 downto 0);
-      max    : out std_logic
+      clk_bcd  : in std_logic;
+      rst_bcd  : in std_logic;
+      ena_bcd  : in std_logic;
+      q_bcd  : out std_logic_vector(N-1 downto 0)
     );
   end component;
 
   signal clk_tb   : std_logic := '0';
   signal rst_tb   : std_logic := '1';
   signal ena_tb   : std_logic := '1';
-  signal count_tb : std_logic_vector(N-1 downto 0);
-  signal max_tb   : std_logic;
+  signal count_tb : std_logic_vector(3 downto 0);
 
 begin
 
-  -- Clock: 40 ns periodo
+  -- Generación del reloj (periodo: 40 ns)
   clk_tb <= not clk_tb after 20 ns;
 
-  -- Reset: activo 80 ns
-  rst_process: process
+  -- Reset inicial por 80 ns
+  process
   begin
     rst_tb <= '1';
     wait for 80 ns;
@@ -41,13 +37,12 @@ begin
 
   -- Instancia del contador
   DUT: BCD_counter
+    generic map(N => 4)
     port map(
-      clk_i  => clk_tb,
-      rst_i  => rst_tb,
-      ena_i  => ena_tb,
-      count  => count_tb,
-      max    => max_tb
+      clk_bcd  => clk_tb,
+      rst_bcd  => rst_tb,
+      ena_bcd  => ena_tb,
+      q_bcd  => count_tb
     );
 
 end architecture;
- 
