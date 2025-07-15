81404f54b1ae96c6ba9b83ff595d7c88d6917128
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 10:20:45 -0300

    Update sum_Nb.vhd

--- bloques_codigo/Codigos/sum_Nb.vhd
+++ bloques_codigo/Codigos/sum_Nb.vhd
@@ -1,38 +1,63 @@
--- Sumador de N bit
-
--- Estudiante: Fernandez, Rocio
+-- Sumador de 4 bits sin generate
+-- Estudiante: Fernández, Rocío
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity sum_Nb is
-	generic(
-		N: natural := 4 -- defino valor default 4bits
-	);
-	port(
-		a_i: in std_logic_vector(N-1 downto 0); -- Vector de entrada a
-		b_i: in std_logic_vector(N-1 downto 0); -- Vector de entrada b
-		c_i: in std_logic; -- Carry de entrada
-		s_o: out std_logic_vector(N-1 downto 0); -- Suma de los vectores
-		c_o: out std_logic -- Carry de salida
-	);
-end;
+entity sum_Nb_4bit is
+  port(
+    a_i : in std_logic_vector(3 downto 0);  -- Entrada A
+    b_i : in std_logic_vector(3 downto 0);  -- Entrada B
+    c_i : in std_logic;                     -- Carry de entrada
+    s_o : out std_logic_vector(3 downto 0); -- Suma
+    c_o : out std_logic                     -- Carry de salida
+  );
+end entity;
 
-architecture sum_Nb_arq of sum_Nb is
-	signal c_aux: std_logic_vector(N downto 0);
+architecture rtl of sum_Nb_4bit is
+  signal c_aux : std_logic_vector(4 downto 0);
 begin
-    c_aux(0) <= c_i;
-	sum_Nb_gen: for i in 0 to N-1 generate
-		sum1b_inst: entity work.sum_1b
-			port map(
-				a_i	=> a_i(i),
-				b_i	=> b_i(i),
-				c_i	=> c_aux(i),
-				s_o	=> s_o(i),
-				c_o 	=> c_aux(i+1)
-			);
-	end generate;
-	-- Conexiones
-	
-	c_o <= c_aux(N);
-end;
+
+  c_aux(0) <= c_i;
+
+  -- Instancias manuales del sumador de 1 bit
+  sum0: entity work.sum_1b
+    port map(
+      a_i => a_i(0),
+      b_i => b_i(0),
+      c_i => c_aux(0),
+      s_o => s_o(0),
+      c_o => c_aux(1)
+    );
+
+  sum1: entity work.sum_1b
+    port map(
+      a_i => a_i(1),
+      b_i => b_i(1),
+      c_i => c_aux(1),
+      s_o => s_o(1),
+      c_o => c_aux(2)
+    );
+
+  sum2: entity work.sum_1b
+    port map(
+      a_i => a_i(2),
+      b_i => b_i(2),
+      c_i => c_aux(2),
+      s_o => s_o(2),
+      c_o => c_aux(3)
+    );
+
+  sum3: entity work.sum_1b
+    port map(
+      a_i => a_i(3),
+      b_i => b_i(3),
+      c_i => c_aux(3),
+      s_o => s_o(3),
+      c_o => c_aux(4)
+    );
+
+  -- Carry final
+  c_o <= c_aux(4);
+
+end architecture;
