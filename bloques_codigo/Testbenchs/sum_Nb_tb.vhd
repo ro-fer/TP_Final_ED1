ac7cecf78d044edf69e8c3a74300dc0c0b711fb9
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 16:53:38 -0300

    Update sum_Nb_tb.vhd

--- bloques_codigo/Testbenchs/sum_Nb_tb.vhd
+++ bloques_codigo/Testbenchs/sum_Nb_tb.vhd
@@ -13,12 +13,18 @@ architecture arch of sum_Nb_tb is
   signal s_o : std_logic_vector(N-1 downto 0);
   signal c_o : std_logic;
 begin
-  -- Instancia del módulo
-  uut: entity work.sum_Nb
-    generic map(N => N)
-    port map(a_i => a_i, b_i => b_i, c_i => c_i, s_o => s_o, c_o => c_o);
+  -- Instancia del mdulo
+  uut: entity work.sum_Nb_4bit
+    
+    port map(
+      a_i => a_i,
+      b_i => b_i, 
+      c_i => c_i,
+      s_o => s_o,
+      c_o => c_o
+    );
 
-  -- Estímulos de entrada
+  -- Estï¿½mulos de entrada
   a_i <= "0011" after 10 ns, "1111" after 40 ns; -- 3 y luego 15
   b_i <= "0001" after 10 ns, "0001" after 40 ns; -- 1 y luego 1
 end arch;
