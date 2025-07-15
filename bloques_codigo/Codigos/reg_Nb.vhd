584950210bf7158510cc688a8f90710e67da30c1
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 10:20:41 -0300

    Update reg_Nb.vhd

--- bloques_codigo/Codigos/reg_Nb.vhd
+++ bloques_codigo/Codigos/reg_Nb.vhd
@@ -1,42 +1,66 @@
--- Registro de N bits --> particularmente 4bits
--- Estudiante: Fernandez, Rocio
+-- Registro con 3 registros internos de 4 bits
+-- Estudiante: Fernández, Rocío
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity reg_Nb is
-  generic(
-    N: natural := 4 -- Bits
+  port (
+    clk_reg : in std_logic; -- Clock
+    rst_reg : in std_logic; -- Reset
+    ena_reg : in std_logic; -- Enable
+
+    -- Entradas
+    d1_reg : in std_logic_vector(3 downto 0);
+    d2_reg : in std_logic_vector(3 downto 0);
+    d3_reg : in std_logic_vector(3 downto 0);
+
+    -- Salidas
+    q1_reg : out std_logic_vector(3 downto 0);
+    q2_reg : out std_logic_vector(3 downto 0);
+    q3_reg : out std_logic_vector(3 downto 0)
   );
-  port(
-    clk_i: in std_logic; -- Clock
-    rst_i: in std_logic; -- Reset
-    ena_i: in std_logic; -- Enable
-    D_reg: in std_logic_vector(N-1 downto 0); -- vector de entrada
-    Q_reg: out std_logic_vector(N-1 downto 0) -- vector de salida
-  );
-end;
+end entity;
 
 architecture reg_Nb_arq of reg_Nb is
-component ffd -- llamo al codigo de Flip Flop D
-  port(
-      clk_i : in std_logic;
-      rst_i : in std_logic;
-      ena_i : in std_logic;
-      d_i   : in std_logic;
-      q_o   : out std_logic
 
-  );
-end component;
+  component registro is
+    port (
+      clk_r : in std_logic;
+      rst_r : in std_logic := '0';
+      ena_r : in std_logic;
+      d_r   : in std_logic_vector(3 downto 0);
+      q_r   : out std_logic_vector(3 downto 0)
+    );
+  end component;
 
 begin
-  ffd_gen: for x in 0 to N-1 generate -- Genero registros de la vant de bits definidos antes
-    ffdx: ffd
-      port map(
-        clk_i => clk_i,
-        rst_i => rst_i,
-        ena_i => ena_i,
-        d_i   => D_reg(x),
-        q_o   => Q_reg(x)
-      );
-  end generate ffd_gen;
-end;
+
+  reg1 : registro
+    port map (
+      clk_r => clk_reg,
+      rst_r => rst_reg,
+      ena_r => ena_reg,
+      d_r   => d1_reg,
+      q_r   => q1_reg
+    );
+
+  reg2 : registro
+    port map (
+      clk_r => clk_reg,
+      rst_r => rst_reg,
+      ena_r => ena_reg,
+      d_r   => d2_reg,
+      q_r   => q2_reg
+    );
+
+  reg3 : registro
+    port map (
+      clk_r => clk_reg,
+      rst_r => rst_reg,
+      ena_r => ena_reg,
+      d_r   => d3_reg,
+      q_r   => q3_reg
+    );
+
+end architecture;
