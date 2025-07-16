60b7f9e7bf10ad4c179bd383776d272abb49d453
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 22:28:27 -0300

    Update bin8_to_bcd.vhd

--- bloques_codigo/Codigos/bin8_to_bcd.vhd
+++ bloques_codigo/Codigos/bin8_to_bcd.vhd
@@ -5,17 +5,17 @@ use IEEE.numeric_std.all;
 
 entity bin8_to_bcd is
   port (
-    bin_i : in  std_logic_vector(7 downto 0);
-    cen_o : out std_logic_vector(3 downto 0);
-    dec_o : out std_logic_vector(3 downto 0);
-    uni_o : out std_logic_vector(3 downto 0)
+    bin_i : in  bit_vector(7 downto 0);
+    cen_o : out bit_vector(3 downto 0);
+    dec_o : out bit_vector(3 downto 0);
+    uni_o : out bit_vector(3 downto 0)
   );
 end entity;
 
 architecture rtl of bin8_to_bcd is
 begin
   process(bin_i)
-    variable shift_reg : std_logic_vector(19 downto 0);
+    variable shift_reg : bit_vector(19 downto 0);
     variable i         : integer;
   begin
     shift_reg := (others => '0');
@@ -23,13 +23,13 @@ begin
 
     for i in 0 to 7 loop
       if unsigned(shift_reg(19 downto 16)) >= 5 then
-        shift_reg(19 downto 16) := std_logic_vector(unsigned(shift_reg(19 downto 16)) + 3);
+        shift_reg(19 downto 16) := bit_vector(unsigned(shift_reg(19 downto 16)) + 3);
       end if;
       if unsigned(shift_reg(15 downto 12)) >= 5 then
-        shift_reg(15 downto 12) := std_logic_vector(unsigned(shift_reg(15 downto 12)) + 3);
+        shift_reg(15 downto 12) := bit_vector(unsigned(shift_reg(15 downto 12)) + 3);
       end if;
       if unsigned(shift_reg(11 downto 8)) >= 5 then
-        shift_reg(11 downto 8) := std_logic_vector(unsigned(shift_reg(11 downto 8)) + 3);
+        shift_reg(11 downto 8) := bit_vector(unsigned(shift_reg(11 downto 8)) + 3);
       end if;
       shift_reg := shift_reg(18 downto 0) & '0';
     end loop;
