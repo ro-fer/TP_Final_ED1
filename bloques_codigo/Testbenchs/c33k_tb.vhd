ee4ddc2afc3fe480bd8cafde0e11bd129bd4bd63
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 23:26:09 -0300

    Update c33k_tb.vhd

--- bloques_codigo/Testbenchs/c33k_tb.vhd
+++ bloques_codigo/Testbenchs/c33k_tb.vhd
@@ -1,40 +1,44 @@
+-- Test de contador de  33k
+
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.bit_1164.all;
 
 entity c33k_tb is
 end;
 
 architecture c33k_tb_arq of c33k_tb is
 
-  component c33k is
-    port (
-      clk_i   : in std_logic;
-      rst_i   : in std_logic;
-      ena_i   : in std_logic;
-      Q_BCD   : out std_logic;
-      cuenta  : out std_logic_vector(21 downto 0)
-    );
-  end component;
+component c33k is 
 
-  signal clk_tb     : std_logic := '0';
-  signal rst_tb     : std_logic := '1';
-  signal ena_tb     : std_logic := '0';
-  signal Q_BCD_tb   : std_logic;
-  signal cuenta_tb  : std_logic_vector(21 downto 0);
+        port (
+            clk_33k : in bit;
+            rst_33k : in bit;   
+            ena_33k : in bit;   
+            out_1   : out bit;  -- se va a habilitar el registro
+            out_2   : out bit;   -- se va a resetear el contador de unos 
+            cuenta  : out bit_vector(15 downto 0)
+        );
+    end component;
 
-begin
+    signal clk_33k_tb   : bit := '0';
+    signal rst_33k_tb   : bit := '0';
+    signal ena_33k_tb   : bit := '0';
+    signal out_1_tb     : bit := '0';
+    signal out_2_tb     : bit := '0';
+    signal salida_tb    : bit_vector(15 downto 0) := (15 downto 0 => '0');
 
-  clk_tb <= not clk_tb after 5 ns;
-  rst_tb <= '1', '0' after 30 ns;
-  ena_tb <= '1' after 10 ns;
+begin 
+    ena_33k_tb <= '1' after 10 ns;
+    clk_33k_tb <= not clk_33k_tb after 5 ns;
+    rst_33k_tb <= '1' after 20 ns, '0' after 50 ns;
 
-  DUT: c33k
-    port map(
-      clk_i   => clk_tb,
-      rst_i   => rst_tb,
-      ena_i   => ena_tb,
-      Q_BCD   => Q_BCD_tb,
-      cuenta  => cuenta_tb
-    );
-
-end architecture;
+    DUT: c33k
+        port map(
+            clk_33k => clk_33k_tb,
+            rst_33k => rst_33k_tb,
+            ena_33k => ena_33k_tb,
+            out_1   => out_1_tb,
+            out_2   => out_2_tb,
+            cuenta  => salida_tb
+        );
+end;
