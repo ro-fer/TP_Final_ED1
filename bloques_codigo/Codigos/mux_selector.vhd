39ecee759f6f8db66d963d8c2e046d865f1fd288
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 22:28:50 -0300

    Create mux_selector.vhd

---
+++ bloques_codigo/Codigos/mux_selector.vhd
@@ -0,0 +1,19 @@
+entity mux_selector is 
+    port(
+        A_selector   : in bit_vector(2 downto 0);
+        B_selector   : in bit_vector(2 downto 0);
+        sel_selector : in bit;
+        sal_selector : out bit_vector(2 downto 0)
+    );
+end;
+
+architecture mux_selector_arq of mux_selector is
+
+begin
+
+    -- si sel_selector = 0 saca A_selector, si sel_selector = 1 saca B_selector
+    sal_selector(0) <= (A_selector(0) and not sel_selector) or (B_selector(0) and sel_selector);
+    sal_selector(1) <= (A_selector(1) and not sel_selector) or (B_selector(1) and sel_selector);
+    sal_selector(2) <= (A_selector(2) and not sel_selector) or (B_selector(2) and sel_selector);
+
+end; 
