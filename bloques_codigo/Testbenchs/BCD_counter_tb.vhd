ce1295be86c5986404d1cbff77bf03fa62bacfc3
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 23:26:15 -0300

    Update BCD_counter_tb.vhd

--- bloques_codigo/Testbenchs/BCD_counter_tb.vhd
+++ bloques_codigo/Testbenchs/BCD_counter_tb.vhd
@@ -1,48 +1,36 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
+-- Test contador BCD
 
-entity BCD_counter_tb is
+entity contador_bcd_tb is
 end;
 
-architecture sim of BCD_counter_tb is
+architecture contador_bcd_tb_arq of contador_bcd_tb is 
 
-  component BCD_counter
-    generic(N: natural := 4);
-    port(
-      clk_bcd  : in std_logic;
-      rst_bcd  : in std_logic;
-      ena_bcd  : in std_logic;
-      q_bcd  : out std_logic_vector(N-1 downto 0)
-    );
-  end component;
+    component contador_bcd is 
 
-  signal clk_tb   : std_logic := '0';
-  signal rst_tb   : std_logic := '1';
-  signal ena_tb   : std_logic := '1';
-  signal count_tb : std_logic_vector(3 downto 0);
+        port(
+            clk_bcd : in bit;
+            ena_bcd : in bit;
+            rst_bcd : in bit;
+            q_bcd   : out bit_vector(3 downto 0)
+        );
+    end component;
 
-begin
+    signal rst_bcd_tb   : bit := '0';
+    signal clk_bcd_tb   : bit := '0';
+    signal ena_bcd_tb   : bit := '0';
+    signal q_bcd_tb     : bit_vector(3 downto 0) := "0000";
 
-  -- Generación del reloj (periodo: 40 ns)
-  clk_tb <= not clk_tb after 20 ns;
+begin 
 
-  -- Reset inicial por 80 ns
-  process
-  begin
-    rst_tb <= '1';
-    wait for 80 ns;
-    rst_tb <= '0';
-    wait;
-  end process;
+    ena_bcd_tb <= '1' after 10 ns, '0' after 100 ns, '1' after 120 ns; 
+    rst_bcd_tb <= '1' after 30 ns, '0' after 50 ns, '1' after 450 ns;
+    clk_bcd_tb <= not clk_bcd_tb after 5 ns;
 
-  -- Instancia del contador
-  DUT: BCD_counter
-    generic map(N => 4)
-    port map(
-      clk_bcd  => clk_tb,
-      rst_bcd  => rst_tb,
-      ena_bcd  => ena_tb,
-      q_bcd  => count_tb
-    );
-
-end architecture;
+    DUT: contador_bcd 
+        port map(
+            clk_bcd => clk_bcd_tb,
+            rst_bcd => rst_bcd_tb,
+            ena_bcd => ena_bcd_tb,
+            q_bcd   => q_bcd_tb
+        );
+end;
