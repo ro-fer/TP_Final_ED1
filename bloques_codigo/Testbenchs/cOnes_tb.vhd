5afeee5ec0a18276280b5b377322e472438a0548
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 23:25:39 -0300

    Update cOnes_tb.vhd

--- bloques_codigo/Testbenchs/cOnes_tb.vhd
+++ bloques_codigo/Testbenchs/cOnes_tb.vhd
@@ -1,41 +1,54 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-
-entity cOnes_tb is
+entity cont_unos_tb is
 end;
 
-architecture arch of cOnes_tb is
+architecture cont_unos_tb_arq of cont_unos_tb is
 
-  signal clk_unos  : std_logic := '0';
-  signal rst_unos  : std_logic := '1';
-  signal ena_unos  : std_logic := '0';
+    component cont_unos is 
+        port (
+            clk_unos:   in bit;
+            rst_unos:   in bit;
+            ena_unos:   in bit;
+            q_bcd1:     out bit_vector(3 downto 0);
+            q_bcd2:     out bit_vector(3 downto 0);
+            q_bcd3:     out bit_vector(3 downto 0);
+            q_bcd4:     out bit_vector(3 downto 0);
+            q_bcd5:     out bit_vector(3 downto 0);
+            q_bcd6:     out bit_vector(3 downto 0);
+            q_bcd7:     out bit_vector(3 downto 0)
+        );
+    end component;
 
-  signal q_bcd1, q_bcd2, q_bcd3, q_bcd4 : std_logic_vector(3 downto 0);
-  signal q_bcd5, q_bcd6, q_bcd7        : std_logic_vector(3 downto 0);
+    signal salida_bcd1_tb :   bit_vector(3 downto 0) := (3 downto 0 => '0');
+    signal salida_bcd2_tb :   bit_vector(3 downto 0) := (3 downto 0 => '0');
+    signal salida_bcd3_tb :   bit_vector(3 downto 0) := (3 downto 0 => '0');
+    signal salida_bcd4_tb :   bit_vector(3 downto 0) := (3 downto 0 => '0');
+    signal salida_bcd5_tb :   bit_vector(3 downto 0) := (3 downto 0 => '0');
+    signal salida_bcd6_tb :   bit_vector(3 downto 0) := (3 downto 0 => '0');
+    signal salida_bcd7_tb :   bit_vector(3 downto 0) := (3 downto 0 => '0');
 
-begin
+    signal clk_unos_tb  : bit := '0';
+    signal rst_unos_tb  : bit := '0';
+    signal ena_unos_tb  : bit := '0';
 
-  -- Instancia del DUT (Device Under Test)
-  uut: entity work.cOnes
-    port map (
-      clk_unos => clk_unos,
-      rst_unos => rst_unos,
-      ena_unos => ena_unos,
-      q_bcd1   => q_bcd1,
-      q_bcd2   => q_bcd2,
-      q_bcd3   => q_bcd3,
-      q_bcd4   => q_bcd4,
-      q_bcd5   => q_bcd5,
-      q_bcd6   => q_bcd6,
-      q_bcd7   => q_bcd7
-    );
+    begin
 
-  -- Clock de 10ns
-  clk_unos <= not clk_unos after 5 ns;
+        ena_unos_tb <= '1' after 10 ns;
+        clk_unos_tb <= not clk_unos_tb after 5 ns;
+        
+        rst_unos_tb <= '1' after 20 ns, '0' after 50 ns;
 
-  -- Reset y Enable
-  rst_unos <= '1', '0' after 20 ns;
-  ena_unos <= '0', '1' after 30 ns;
-
-end architecture;
+        DUT: cont_unos
+            port map (
+                clk_unos    => clk_unos_tb,
+                rst_unos    => rst_unos_tb,
+                ena_unos    => ena_unos_tb,
+                q_bcd1      => salida_bcd1_tb,
+                q_bcd2      => salida_bcd2_tb,
+                q_bcd3      => salida_bcd3_tb,
+                q_bcd4      => salida_bcd4_tb,
+                q_bcd5      => salida_bcd5_tb,
+                q_bcd6      => salida_bcd6_tb,
+                q_bcd7      => salida_bcd7_tb
+            );
+    end;
+    
