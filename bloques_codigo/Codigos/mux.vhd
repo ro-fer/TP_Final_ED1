e3a11998d10f0085f748018a57dd63c04a3ec6f4
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 22:29:05 -0300

    Update mux.vhd

--- bloques_codigo/Codigos/mux.vhd
+++ bloques_codigo/Codigos/mux.vhd
@@ -3,62 +3,69 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity mux is
-  port (
-    clk_i      : in std_logic;
-    rst_i      : in std_logic;
-    pix_x_i    : in std_logic_vector(9 downto 0);
-    pix_y_i    : in std_logic_vector(9 downto 0);
-    video_on_i : in std_logic;
-    char0      : in std_logic_vector(7 downto 0);
-    char1      : in std_logic_vector(7 downto 0);
-    char2      : in std_logic_vector(7 downto 0);
-    char3      : in std_logic_vector(7 downto 0);
-    char4      : in std_logic_vector(7 downto 0);
-    pixel_o    : out std_logic;
-    char_sel_o : out std_logic_vector(7 downto 0);
-    sub_fila_o : out std_logic_vector(2 downto 0); 
-    sub_col_o  : out std_logic_vector(2 downto 0)   
-  );
-end entity;
-
-architecture rtl of mux is
-  -- Subdivisiones 
-  signal char_sel     : std_logic_vector(7 downto 0); -- Caracter seleccionado
-  signal char_addr    : std_logic_vector(3 downto 0); -- Dirección del caracter (4 bits)
-  signal sub_fila     : std_logic_vector(2 downto 0); -- Y interna del caracter (bits 6-4)
-  signal sub_col      : std_logic_vector(2 downto 0); -- X interna del caracter (bits 2-0)
-  signal rom_pixel    : std_logic; -- Salida de la ROM de caracteres
-begin
-
-  -- Dividir las coordenadas
-  char_addr <= pix_x_i(7 downto 4);
-  sub_col   <= pix_x_i(2 downto 0);
-  sub_fila  <= pix_y_i(6 downto 4);
-
-  -- Salida para poder ver sub_fila y sub_col en simulación
-  sub_fila_o <= sub_fila;
-  sub_col_o  <= sub_col;
-
-  -- Selector de caracter según zona horizontal
-  with char_addr select
-    char_sel <= char0 when "0000",
-                char1 when "0001",
-                char2 when "0010",
-                char3 when "0011",
-                char4 when "0100",
-                "00000000" when others;
-
-  -- Instancia de la ROM de caracteres
-  rom_inst: entity work.rom
-    port map (
-      char_address => char_addr,  -- Acá va char_addr de 4 bits (no char_sel)
-      sub_fila     => sub_fila,
-      sub_col      => sub_col,
-      rom_data     => rom_pixel
+    port(
+        bcd1    : in  bit_vector(3 downto 0);
+        punto   : in  bit_vector(3 downto 0); -- '.'
+        bcd2    : in  bit_vector(3 downto 0);
+        bcd3    : in  bit_vector(3 downto 0);
+        voltaje : in  bit_vector(3 downto 0); -- 'V'
+        espacio : in  bit_vector(3 downto 0); -- ' ' 
+        selector: in  bit_vector(2 downto 0); -- selecciona una de las 6 entradas
+        salida  : out bit_vector(3 downto 0)  -- se va a la ROM
     );
+end;
 
-  -- Salida de color blanco si hay video y el pixel está activo
-  pixel_o <= rom_pixel and video_on_i;
-  char_sel_o <= char_sel;
+architecture mux_arq of mux is 
 
-end architecture;
+    signal salida_bcd1      : bit_vector(3 downto 0);
+    signal salida_punto     : bit_vector(3 downto 0);
+    signal salida_bcd2      : bit_vector(3 downto 0);
+    signal salida_bcd3      : bit_vector(3 downto 0);
+    signal salida_voltaje   : bit_vector(3 downto 0);
+    signal salida_espacio   : bit_vector(3 downto 0);
+
+begin
+    --                               "   0                       0                   0 "
+    salida_bcd1(0) <= bcd1(0) and (not(selector(2)) and not(selector(1)) and not (selector(0)));
+    salida_bcd1(1) <= bcd1(1) and (not(selector(2)) and not(selector(1)) and not (selector(0)));
+    salida_bcd1(2) <= bcd1(2) and (not(selector(2)) and not(selector(1)) and not (selector(0)));
+    salida_bcd1(3) <= bcd1(3) and (not(selector(2)) and not(selector(1)) and not (selector(0)));
+
+    --                               "   0                       0                   1 "
+    salida_punto(0) <= punto(0) and (not(selector(2)) and not(selector(1)) and (selector(0)));
+    salida_punto(1) <= punto(1) and (not(selector(2)) and not(selector(1)) and (selector(0)));
+    salida_punto(2) <= punto(2) and (not(selector(2)) and not(selector(1)) and (selector(0)));
+    salida_punto(3) <= punto(3) and (not(selector(2)) and not(selector(1)) and (selector(0)));
+
+    --                               "   0                       1                   0 "
+    salida_bcd2(0) <= bcd2(0) and (not(selector(2)) and (selector(1)) and not (selector(0)));
+    salida_bcd2(1) <= bcd2(1) and (not(selector(2)) and (selector(1)) and not (selector(0)));
+    salida_bcd2(2) <= bcd2(2) and (not(selector(2)) and (selector(1)) and not (selector(0)));
+    salida_bcd2(3) <= bcd2(3) and (not(selector(2)) and (selector(1)) and not (selector(0)));
+
+    --                               "   0                       1                   1 "
+    salida_bcd3(0) <= bcd3(0) and (not(selector(2)) and (selector(1)) and (selector(0)));
+    salida_bcd3(1) <= bcd3(1) and (not(selector(2)) and (selector(1)) and (selector(0)));
+    salida_bcd3(2) <= bcd3(2) and (not(selector(2)) and (selector(1)) and (selector(0)));
+    salida_bcd3(3) <= bcd3(3) and (not(selector(2)) and (selector(1)) and (selector(0)));
+
+    --                               "   1                       0                   0 "
+    salida_voltaje(0) <= voltaje(0) and ((selector(2)) and not(selector(1)) and not (selector(0)));
+    salida_voltaje(1) <= voltaje(1) and ((selector(2)) and not(selector(1)) and not (selector(0)));
+    salida_voltaje(2) <= voltaje(2) and ((selector(2)) and not(selector(1)) and not (selector(0)));
+    salida_voltaje(3) <= voltaje(3) and ((selector(2)) and not(selector(1)) and not (selector(0)));
+
+
+    --                               "   1                       0                   1 "            "   1                 1                   1 "       "   1                 1                   0"
+    salida_espacio(0) <= espacio(0) and (((selector(2)) and not(selector(1)) and (selector(0))) or (selector(2) and selector(1) and selector(0)) or (selector(2) and selector(1) and not selector(0)));
+    salida_espacio(1) <= espacio(1) and (((selector(2)) and not(selector(1)) and (selector(0))) or (selector(2) and selector(1) and selector(0)) or (selector(2) and selector(1) and not selector(0)));
+    salida_espacio(2) <= espacio(2) and (((selector(2)) and not(selector(1)) and (selector(0))) or (selector(2) and selector(1) and selector(0)) or (selector(2) and selector(1) and not selector(0)));
+    salida_espacio(3) <= espacio(3) and (((selector(2)) and not(selector(1)) and (selector(0))) or (selector(2) and selector(1) and selector(0)) or (selector(2) and selector(1) and not selector(0)));
+
+    --
+    salida(0) <= salida_bcd1(0) or salida_punto (0) or salida_bcd2(0) or salida_bcd3(0) or salida_voltaje(0) or salida_espacio(0);
+    salida(1) <= salida_bcd1(1) or salida_punto (1) or salida_bcd2(1) or salida_bcd3(1) or salida_voltaje(1) or salida_espacio(1);
+    salida(2) <= salida_bcd1(2) or salida_punto (2) or salida_bcd2(2) or salida_bcd3(2) or salida_voltaje(2) or salida_espacio(2);
+    salida(3) <= salida_bcd1(3) or salida_punto (3) or salida_bcd2(3) or salida_bcd3(3) or salida_voltaje(3) or salida_espacio(3);
+
+end;
