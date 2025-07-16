800085a657937bf2694ed291b5f54255736d45ff
Rocio Fernandez <rfernandez@estudiantes.unsam.edu.ar>
2025-07-15 23:25:41 -0300

    Update comp_Nb_tb.vhd

--- bloques_codigo/Testbenchs/comp_Nb_tb.vhd
+++ bloques_codigo/Testbenchs/comp_Nb_tb.vhd
@@ -1,13 +1,13 @@
 -- Test comparador de N bits
 
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.bit_1164.all;
 
 entity comp_Nb_tb is end;
 architecture arch of comp_Nb_tb is
   constant N : natural := 4;
-  signal a, b : std_logic_vector(N-1 downto 0);
-  signal s : std_logic;
+  signal a, b : bit_vector(N-1 downto 0);
+  signal s : bit;
 begin
   uut: entity work.comp_Nb
     generic map(N => N)
